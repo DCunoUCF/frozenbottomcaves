15da6a4c0f8e0768f90cda43a8e62d4632cfea4d
fibster4 <naas.christopher1@gmail.com>
2020-02-10 16:46:11 -0500

    Mouseover fills highlights

--- Assets/Scripts/selectTile.cs
+++ Assets/Scripts/selectTile.cs
@@ -4,8 +4,26 @@ using UnityEngine;
 
 public class selectTile : MonoBehaviour
 {
+    private bool filled = false;
+    private GameObject fill ;
+
     private void OnMouseDown()
     {
         PlayerManager.Instance.setSelectedTile(transform.parent.position);
+    }
+
+    private void OnMouseOver()
+    {
+        if(!filled)
+        {
+            transform.GetChild(0).gameObject.SetActive(true);
+        }
+        filled = true;
+    }
+
+    private void OnMouseExit()
+    {
+        transform.GetChild(0).gameObject.SetActive(false);
+        filled = false;
     }
 }
