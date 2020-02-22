28d66a7995f94c8fe33189a508abc330ae437063
Richard B Gullo <gullorichardb@gmail.com>
2020-02-22 15:32:57 -0500

    Scrollable Dialogue
    Scrollable Dialogue implemented.

---
+++ Assets/Scripts/New Dialogue Scripts/InitializeDialogue.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InitializeDialogue : MonoBehaviour
+{
+    public GameObject DialoguePanel;
+    public GameObject TextBox;
+    public Button[] Choices;
+    public Dialogue dialogue;
+
+    public GameObject getTextBox()
+    {
+        return TextBox;
+    }
+
+    public Button[] getChoices()
+    {
+        return Choices;
+    }
+
+    public Button getChoice01()
+    {
+        return Choices[0];
+    }
+
+    public Button getChoice02()
+    {
+        return Choices[1];
+    }
+
+    public Button getChoice03()
+    {
+        return Choices[2];
+    }
+}
