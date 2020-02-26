e2629d8fd4e51dcf1a9888ac221d5c2fb24c89a5
DCunoUCF <46467763+DCunoUCF@users.noreply.github.com>
2020-02-26 15:59:22 -0500

    Update WorldNode.cs

--- Assets/Scripts/WorldNode.cs
+++ Assets/Scripts/WorldNode.cs
@@ -8,16 +8,28 @@ public enum FlagType
 	Battle, Event, Item
 }
 
+public enum GridType
+{
+    Random = 0, Forest, Cave, Ice, Castle
+}
+
 public enum EncounterEnemy
 {
 	None,
-	Goblin, Slime
+	Goblin, Slime_G, Gnoll, Lich
 }
 
 public enum ItemType
 {
 	None,
 	DwarfKey
+}
+
+public struct BattleStruct
+{
+    public GridType grid;
+    public string arena;
+	public List<EncounterEnemy> nodeEnemies;
 }
 
 // TODO: Make a struct that contains all the node information, 
@@ -27,7 +39,7 @@ public class WorldNode : MonoBehaviour
 {
 	public List<int> NodeIDs;
 	public List<FlagType> NodeTypes;
-	public List<List<EncounterEnemy>> NodeEnemies;
+    public List<BattleStruct> battleStruct;
 	public List<ItemType> NodeItems;
 
     // Start is called before the first frame update
