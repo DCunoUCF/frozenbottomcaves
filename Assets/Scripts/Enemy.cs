ff40210c31c58252f2cddb8b5f449c0c77a82fe8
MrJellimann <46575936+MrJellimann@users.noreply.github.com>
2020-03-01 14:10:42 -0500

    Pushing enemy skeletons

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Enemy : MonoBehaviour
+{
+	protected int enemyId;
+	protected char enemyRole;
+	protected int health;
+	protected int damage;
+	protected int strength;
+	protected Vector3 gridPosition;
+	protected Vector3 moveTarget;
+	protected Vector3 attackTarget;
+	protected int decision; // 0 - no decision, 1 - move, 2 - attack
+
+	//===========   Unity Methods   ============//
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //===========   Other methods   ===========//
+    protected abstract void initialize();
+
+    private void generateID()
+    {
+    	this.enemyId = (int)(Random.value * 9999999);
+    }
+
+    //===========   Thinking methods   ===========//
+    protected abstract void move();
+    protected abstract void attack();
+    public abstract void decide();
+
+    public void kill()
+    {
+    	//
+    	Debug.Log("Oy blyat! E#"+this.enemyId+" am dead");
+    }
+
+    public void dealDamage(int dam)
+    {
+    	this.health -= dam;
+
+    	if (this.health <= 0)
+    		kill();
+    }
+
+    //===========   Getters   ===========//
+    public int getHealth() { return this.health; }
+    public int getStrength() { return this.strength; }
+    public int getDamage() { return this.damage; }
+    public int getID() { return this.enemyId; }
+    public int getDecision() { return this.decision; }
+    public Vector3 getMoveVector() { return this.moveTarget; }
+    public Vector3 getAttackVector() { return this.attackTarget; }
+    public char getRole() { return this.enemyRole; }
+}
---
+++ Assets/Scripts/EnemyDunce.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDunce : Enemy
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.initialize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //=============   You Did This To Me David   =============//
+    protected override void initialize()
+    {
+    	// Set type
+    	this.enemyRole = 'd';
+
+    	// Set stats
+    	this.health = 2;
+    	this.damage = 1;
+    	this.strength = 1;
+
+    	// Set positions
+    	this.gridPosition = new Vector3(0, 0, 0);
+    	this.attackTarget = new Vector3(0, 0, 0);
+    	this.moveTarget = new Vector3(0, 0, 0);
+
+    	// Generate ID
+    	this.generateID();
+    }
+
+    protected override void move()
+    {
+    	Debug.Log("E#"+this.enemyId+" is moving!");
+    }
+
+    protected override void attack()
+    {
+    	Debug.Log("E#"+this.enemyId+" is attacking!");
+    }
+
+    public override void decide()
+    {
+    	Debug.Log("E#"+this.enemyId+" has been called upon to think!");
+
+    	if (Random.value < .5f)
+    		this.move();
+    	else
+    		this.attack();
+    }
+
+}
