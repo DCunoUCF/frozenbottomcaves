5c0c62ff46de53c99d70a6a6797ed6f3931a358e
Richard B Gullo <gullorichardb@gmail.com>
2020-03-18 22:03:50 -0400

    Added Inventory System
    Inventory System is added and should be working well.

---
+++ Assets/Scripts/InventoryScripts/PlayerManager2.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager2 : MonoBehaviour
+{
+    public Player2 player;
+    public Inventory inventory;
+    public UIInventory inventoryUI;
+    public GameObject InventoryPanel;
+    public GameObject BioPanel;
+ 
+
+    void Start()
+    {
+        player = new Player2("Mage");
+        player.inventory = inventory;
+        player.inventory.updateStats(player);
+        player.inventory.addItem(Item.ItemType.Sword, 5);
+        player.inventory.addItem(Item.ItemType.Ressurection, 3);
+        player.inventory.addItem(Item.ItemType.Provisions, 5);
+        player.inventory.addItem(Item.ItemType.Gold, 100);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            if (inventoryUI.gameObject.activeSelf)
+            {
+                inventoryUI.gameObject.SetActive(false);
+            }
+            else
+            {
+                inventory.updateStats(player);
+                InventoryPanel.SetActive(true);
+                BioPanel.SetActive(false);
+                inventoryUI.gameObject.SetActive(true);
+            }
+        }
+    }
+
+
+
+}
